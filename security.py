497e588a8e6a0ce22f7e09a5f323fcecc70f0dc6
Victor Domingos <victordomingos@users.noreply.github.com>
2017-12-15 00:12:29 +0000

    Update security.py

--- security.py
+++ security.py
@@ -1,8 +1,11 @@
 from user import User
+from werkzeug.security import safe_str_cmp
+
 
 # Table users (pseudo DB)
 users = {
-    User(1, 'Bob', 'asdf')
+    User(1, 'Bob', 'asdf'),
+    User(2, 'victor', '1234'),
 }
 
 
@@ -14,7 +17,7 @@ username_mapping = { 'Bob': {
     'id': 1
     'username': 'Bob'
     'password': 'asdf'
-    }    
+    }
 }
 
 
@@ -23,17 +26,17 @@ userid_mapping = { 1: {
     'id': 1
     'username': 'Bob'
     'password': 'asdf'
-    }    
+    }
 }
 """
 
 
 def authenticate(username, password):
     user = username_mapping.get(username, None)  # By default, returns None in case no username provided
-    if user and user.password == password:
+    if user and safe_str_cmp(user.password, password):
         return user
 
 
 def identity(payload):
     user_id = payload['identity']
-    return user_id_mapping.get(user_id, None)
+    return userid_mapping.get(user_id, None)

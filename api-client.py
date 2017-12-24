14050655f5b76ed4cf495b14f851d8519d046ce4
victordomingos <>
2017-12-24 01:29:12 +0000

    small refinement

--- api-client.py
+++ api-client.py
@@ -3,13 +3,15 @@
 import requests
 import json
 
+IP = '172.20.10.10'
+API_URL = 'http://'+IP+':5000'
 
 def get_repairs():
-    api_URL = 'http://localhost:5000/repairs'
+    url = API_URL+'/repairs'
     try:
         params = {}
 
-        repairs = requests.get(api_URL, params=params, timeout=(2, 5)).json()
+        repairs = requests.get(url, params=params, timeout=(2, 5)).json()
         return repairs
 
     except Exception as e:
@@ -17,12 +19,12 @@ def get_repairs():
 
 
 def get_repair(num_rep, jwtoken):
-    api_URL = 'http://localhost:5000/repair/'+str(num_rep)
+    url = API_URL+'/repair/'+str(num_rep)
     token = "JWT " + jwtoken
     try:
         params = {}
         headers = {'Authorization': token}
-        repair = requests.get(api_URL,
+        repair = requests.get(url,
                               params=params,
                               headers=headers, 
                               timeout=(2, 5)
@@ -34,32 +36,24 @@ def get_repair(num_rep, jwtoken):
 
 
 def post_authentication(username, password):
+    url = API_URL + '/auth'
     data = json.dumps({"username": username, "password": password})
     headers = {"Content-Type": "application/json"}
     
-    r = requests.post('http://localhost:5000/auth', 
-                       data=data,
-                       headers=headers,
-                       timeout=(2,5)
-                       )
+    r = requests.post(url, 
+                      data=data,
+                      headers=headers,
+                      timeout=(2,5)
+                      )
     response = r.json()
     return response['access_token']    #set token value
     
-    
-    #Now you can do authorised calls
-    #r = requests.get('http://localhost:5000/protected', 
-    #                headers={'Authentication-Token': token})
-    #print(r.text)
-
 
 print("Repairs: ", get_repairs())
-#print("---")
-#print("Repair number 1234:", get_repair(1234))
 print("---")
 my_new_jwt = post_authentication("victor", "1234")
 print("JWT: ", my_new_jwt)
 print("---")
 print("Repair number 1234:", get_repair(1234, my_new_jwt))
 print("---")
- 
-print("---")
+
